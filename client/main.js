538315525136fead3b6f0ebdd9ff49fee0838859
Doug Scheible <doug_scheible@yahoo.com>
2025-01-11 18:31:59 -0600

    initial commit

--- client/main.js
+++ client/main.js
@@ -1,5 +1,7 @@
+// index.js (React 18+)
 import React from "react";
-import { render } from "react-dom";
+import { createRoot } from "react-dom/client"; // Correct import
 import HelloWorld from "./HelloWorld";
 
-render(<HelloWorld />, document.getElementById("root"));
+const root = createRoot(document.getElementById("root")); // Create root node
+root.render(<HelloWorld />); // Render component
